30650787dd5d1cee749386899c5b8c1a08122fb4
matt-muniz <45434380+matt-muniz@users.noreply.github.com>
2018-12-27 16:32:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<hmtl>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Serendipity's Playhouse</title>
+		<link href="css/main.css" type="text/css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="js/lettering.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="navbar-slider" id="sid-navbar-silder"></div><!-- side navbar slider -->
+			<div id="side-navbar">
+					<div id="side-navbar-logo">
+						<img src="img/spp_logo_new.png">
+					</div><!-- logo -->
+					<div class="social-icons" id="side-navbar-social">
+						<img src="img/fb_gray.png">
+					</div><!-- social icons -->
+					<div id="daily-adm">
+						<ul>
+							<p><strong>Daily Admissions</strong></p>
+							<li>$12 Per Child</li>
+							<li>$6 Additional Sibling</li>
+							<form action="daily_adm/daily_adm.php">
+								<button type="submit" class="btn" id="daily-btn">More</button>
+							</form>
+						</ul>
+					</div><!-- daily admissions -->
+					<div id="hours">
+						<ul>
+							<p><strong>Hours</strong></p>
+							<li class="wk-day">Monday - Friday</li>
+							<li>9:00AM - 7:00Pm</li>
+							<li class="wk-day">Saturday</li>
+							<li>10:00AM - 5:00Pm</li>
+							<li class="wk-day">Sunday</li>
+							<li>1:00PM - 3:00PM</li>
+							<li>4:00PM - 6:00PM</li>						
+						</ul>
+					</div><!-- hours -->
+			</div><!-- side-navbar -->
+		</div> <!-- Container -->
+		<div class="container">
+			<div id="nav-bar">
+				<div id="nav-bar-img">
+					<img src="img/spp_logo_new.png">
+				</div>
+				<div class="topnav" id="myTopnav">
+					<a href="#home">Home</a>
+					<a href="#news">About us</a>
+					<a href="#contact">Zumbini</a>
+					<div class="dropdown"></div> 
+					<a href="#about">Birthday Parties</a>
+					<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+				</div>
+			</div><!-- nav bar -->
+		</div><!-- container -->
+		<div class="container">
+			<main id="main-body">
+				<div class="badge">
+				<h1>CLEAN SAFE FUN</h1>
+				</div>
+				<h1>Grand Opening Coming Soon</h1>
+				<p>Follow us on Facebook</p>
+				<p>for More Updates!</p>
+				<h2>The Meaning of Serendipity:</h2>
+				<p style="color: #00BFFF">Looking for something and finding something even better</p>
+			</main><!-- main body -->
+		</div><!-- container -->
+		<div class="container">
+			<div class="navbar-slider" id="packages-silder"></div><!-- side navbar slider -->
+			<main id="packages">
+				<h1>$500 Birthday Party Package Will Include</h1>
+				<ul>
+					<li>Up to 20 Kids</li>
+					<li>$10 Each Additional Kid</li>
+					<li>2 Hour Private Party</li>
+					<li>30 Minute Setup Time Before</li>
+					<li>Water and Snacks Available for 20 Kids </li>
+					<li>20 Goodie Bags</li>
+					<li>Tablecloths, Plages and Napkins</li>
+					<li>2 of your Favorite  Characters</li>
+					<li>Half Sheet Cake, Chocolate or Vanilla</li>
+				</ul>
+				<form action="#">
+					<button type="submit" class="btn" id="packages-btn">More Packages</button>
+				</form>
+			</main>
+		</div>
+		<div class="container">
+			<div class="navbar-slider" id="calender-silder"></div><!-- side navbar slider -->
+			<div id="calender-container">
+				
+			</div>
+		</div>
+		<script src="js/script.js"></script>
+	
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,10 @@
+var h1 = $("h1").lettering();
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
